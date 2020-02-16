0887a599a6f3f1ca7e3b6fbecd9e0860a2ff5fb5
susanwen4no <56980359+susanwen4no@users.noreply.github.com>
2020-02-16 02:01:22 -0500

    form data -> csv
    One of them should work? (thank stack overflow) but only one is needed probably.

---
+++ inputStorage/inputStorage.js
@@ -0,0 +1,25 @@
+//exporting input from candidate side form into a csv file
+
+var name = document.getElementById("name");
+
+var issues = ["Foreign Policy", "Abortion", "Education"];
+// the list of issues is hardcoded
+
+var rows = [["Name", "Issue", "Opinion", "Rank"]]
+
+for(i = 0, a = 0; i < issues.length; i ++, a+= 2){
+	rows.push([name, issues[0],
+	document.getElementById("candidate_side").elements[3+a],
+	document.getElementById("candidate_side").elements[4+a]]);
+	
+	// first 3 elements in form are name photo bio
+	// following are pairs of opinion + rank inputs
+}
+
+// I don't actually really understand how this works, but stackoverflow
+// is my friend
+let csvContent = "data:text/csv;charset=utf-8," 
+    + rows.map(e => e.join(",")).join("\n");
+	
+var encodedUri = encodeURI(csvContent);
+window.open(encodedUri);
