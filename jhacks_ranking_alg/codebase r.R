edcb763207400ffe2c42d9d4db3074372a22b50d
Kevin Wu <42019030+Grasstown@users.noreply.github.com>
2020-02-16 00:53:56 -0500

    Ranking algorithm for candidates to user

---
+++ jhacks_ranking_alg/codebase r.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(readr)
+library(pracma)
+
+poli <- read_csv("C:/Users/Alex/Google Drive/jhacks/polidummydata.csv")
+user <- read_csv("C:/Users/Alex/Google Drive/jhacks/userdummydata.csv")
+
+enddata <- dplyr::left_join(poli, user)
+
+norm<- function(nums){
+  sum<- 0
+  for(i in nums){
+    sum <- sum + i^2
+  }
+  return(sqrt(sum))
+}
+
+enddata2 <- enddata %>%
+  group_by(Name) %>%
+  mutate(dotprod= dot(userscore, poliscore)) %>%
+  mutate(mag1 = norm(userscore)) %>%
+  mutate(mag2 = norm(poliscore)) %>%
+  mutate(index = dotprod/(mag1*mag2))
+  
