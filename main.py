341c717ea9c3792fe509db89c884f47d85eb5ede
Kevin Wu <42019030+Grasstown@users.noreply.github.com>
2020-02-16 12:48:34 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,25 +5,33 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return render_template('landing.html')
+
 @app.route('/submit', methods = ['POST'])
 def submit():
     if request.method == 'POST':
         result = request.form
         f = request.files['photo']
-        filename = result.get('name') + secure_filename(f.filename)
-        f.save(os.path.join('./uploadedFiles', filename))
+        filename = result.get('name') + '_' + secure_filename(f.filename)
+        filepath = os.path.join('./uploadedFiles', filename)
+        f.save(filepath)
         
         c = open('./candidateData/' + result.get('name') + '.csv', 'w+')
 
-        line1String = '';
-        line2String = '';
+        lineTitle = 'name, photo, issue, opinion on issue, issue importance rank\r\n'
+        lineValues = result.get('name') + ',' + filepath + ','
+        prefixLength = len(lineValues)
 
         for key in result.keys():
-            line1String += key + ','
-        for value in result.values():
-            line2String += value + ','
+            if key.endswith('Stance'):
+                keyL = len(key)
+                lineValues += key[:-6] + ',' + result.get(key)
+            elif key.endswith('Importance'):
+                lineValues += ',' + result.get(key) + '\r\n' + result.get('name') + ',' + filepath + ','
 
-        c.write(line1String.rstrip(',') + '\r\n' + line2String.rstrip(','))
+        c.write(lineTitle + lineValues[:-prefixLength])
 
         return render_template('submit.html', result = result)
 
